f9fe4f54d627d37ca26a30e4da1f3af6f656fd79
Shakutaigit <ron.shakutai@gmail.com>
2020-04-30 13:30:07 +0300

    the begining of the menu .

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ $(function() {
 
 	});
 });
+// Loginnnnnnnnnnnnn
 function UserAndPassConfirm(){
 	var txtbox1=document.getElementById("txtbox1");
 	var txtbox2=document.getElementById("txtbox2");
@@ -82,6 +83,41 @@ function UserAndPassConfirm(){
 		alert(" Welcome , your UserName is incorrect");
 	}
 }
+/////// colors picker
+$( function() {
+	function hexFromRGB(r, g, b) {
+		var hex = [
+			r.toString( 16 ),
+			g.toString( 16 ),
+			b.toString( 16 )
+		];
+		$.each( hex, function( nr, val ) {
+			if ( val.length === 1 ) {
+				hex[ nr ] = "0" + val;
+			}
+		});
+		return hex.join( "" ).toUpperCase();
+	}
+	function refreshSwatch() {
+		var red = $( "#red" ).slider( "value" ),
+			green = $( "#green" ).slider( "value" ),
+			blue = $( "#blue" ).slider( "value" ),
+			hex = hexFromRGB( red, green, blue );
+		$( "#swatch" ).css( "background-color", "#" + hex );
+	}
+
+	$( "#red, #green, #blue" ).slider({
+		orientation: "horizontal",
+		range: "min",
+		max: 255,
+		value: 127,
+		slide: refreshSwatch,
+		change: refreshSwatch
+	});
+	$( "#red" ).slider( "value", 255 );
+	$( "#green" ).slider( "value", 140 );
+	$( "#blue" ).slider( "value", 60 );
+} );
 
 function addUser() {
 	var user = document.getElementById("user").value;
